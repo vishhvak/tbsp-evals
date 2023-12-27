9c310075f7d2ada19c48fe15feb91edc1dc3da91
jeremyphilemon <git@jrmyphlmn.com>
2023-12-27 17:29:06 +0300

    Add categories

--- scripts/eval-alpha.py
+++ scripts/eval-alpha.py
@@ -1,21 +1,25 @@
-import os
-import pandas as pd
-import json 
-from openai import OpenAI
-import sys 
 import argparse
+import json
+import os
+import sys
+
+import pandas as pd
+from openai import OpenAI
 from tqdm import tqdm
+
 tqdm.pandas()
 # os.environ['OPENAI_API_KEY'] = "insert your key here"
 
 
 def create_chat_prompt(item_description):
-    example_schema = {"properties": {"foo": {"title": "Foo", "description": "a list of strings", "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}
+    example_schema = {"properties": {"foo": {"title": "Foo", "description": "a list of strings",
+                                             "type": "array", "items": {"type": "string"}}}, "required": ["foo"]}
     example_object = {"foo": ["bar", "baz"]}
     incorrect_object = {"properties": {"foo": ["bar", "baz"]}}
-    output_schema = {"properties": {"carbohydrates": {"title": "Carbohydrates", "description": "carbohydrates in grams", "type": "number"}, "proteins": {"title": "Proteins", "description": "proteins in grams", "type": "number"}, "fats": {"title": "Fats", "description": "fats in grams", "type": "number"}, "calories": {"title": "Calories", "description": "calories in kcal", "type": "number"}}, "required": ["carbohydrates", "proteins", "fats", "calories"]}
+    output_schema = {"properties": {"carbohydrates": {"title": "Carbohydrates", "description": "carbohydrates in grams", "type": "number"}, "proteins": {"title": "Proteins", "description": "proteins in grams", "type": "number"}, "fats": {
+        "title": "Fats", "description": "fats in grams", "type": "number"}, "calories": {"title": "Calories", "description": "calories in kcal", "type": "number"}}, "required": ["carbohydrates", "proteins", "fats", "calories"]}
 
-    user_prompt = f"""what is the nutrition information of 100g of {item_description}?
+    user_prompt = f"""What is the nutrition information of 100g of {item_description}?
     The output should be formatted as a JSON instance that conforms to the JSON schema below.
 
     As an example, for the schema {example_schema}
@@ -23,10 +27,11 @@ def create_chat_prompt(item_description):
 
     Here is the output schema:
     {output_schema}
-    """ 
+    """
     return [
         {"role": "user", "content": user_prompt}
     ]
+
 
 def is_valid_json(json_data):
     try:
@@ -34,6 +39,7 @@ def is_valid_json(json_data):
     except ValueError as e:
         return False
     return True
+
 
 def compare_jsons(json1, json2):
     if not is_valid_json(json1) or not is_valid_json(json2):
@@ -47,10 +53,11 @@ def compare_jsons(json1, json2):
             return False, "Values mismatch for key: " + key
     return True, "Match"
 
+
 def get_answer(messages):
     response = client.chat.completions.create(
-        model="gpt-4-1106-preview", #gpt-3.5-turbo-1106
-        response_format={ "type": "json_object" },
+        model="gpt-4-1106-preview",  # gpt-3.5-turbo-1106
+        response_format={"type": "json_object"},
         temperature=0.0,
         seed=42,
         messages=messages
@@ -58,44 +65,49 @@ def get_answer(messages):
     # TODO: Add logic to verify system_fingerprint is same across all requests
     return response.choices[0].message.content
 
+
 if __name__ == "__main__":
-    
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--sample_number", type=int, default=100)
     parser.add_argument("--dataset", type=str, default="sr28.csv")
     parser.add_argument("--output_file", type=str, default="eval_result.json")
     args = parser.parse_args()
 
-
     df = pd.read_csv(args.dataset)
 
-    test_samples = df.sample(args.sample_number, random_state=1).reset_index(drop=True)
-    test_samples["input"] = test_samples.apply(lambda x: create_chat_prompt(x['description']), axis=1)
+    test_samples = df.sample(
+        args.sample_number, random_state=1).reset_index(drop=True)
+    test_samples["input"] = test_samples.apply(
+        lambda x: create_chat_prompt(x['description']), axis=1)
 
     client = OpenAI()
 
-    test_samples['gpt-response'] = test_samples.input.progress_apply(get_answer)
+    test_samples['gpt-response'] = test_samples.input.progress_apply(
+        get_answer)
 
     # Calculate overall accuracy of match between estimated JSON and Ground Truth JSON of Nutrients
     # 1. Filter out the invalid JSONs
     # 2. Key-wise comparison of the JSONs
     # 3. Calculate the accuracy
-    test_samples['correct'], test_samples['reason'] = zip(*test_samples.apply(lambda row: compare_jsons(row['answer'], row['gpt-response']), axis=1))
-
+    test_samples['correct'], test_samples['reason'] = zip(
+        *test_samples.apply(lambda row: compare_jsons(row['answer'], row['gpt-response']), axis=1))
 
     # Export the result as a csv
     # test_samples.to_csv('eval_result.csv', index=False)
-    
+
     # Export the result as a json
     test_samples['item_description'] = test_samples['description']
-    test_samples['nutrients'] = test_samples['answer'].apply(lambda x: {k: v for k, v in sorted(json.loads(x).items(), key=lambda item: item[0])})
-    test_samples['estimated_nutrients'] = test_samples['gpt-response'].apply(lambda x: {k: v for k, v in sorted(json.loads(x).items(), key=lambda item: item[0])})
-    keys_to_export = ['item_description', 'nutrients', 'estimated_nutrients', 'correct', 'reason']
+    test_samples['nutrients'] = test_samples['answer'].apply(
+        lambda x: {k: v for k, v in sorted(json.loads(x).items(), key=lambda item: item[0])})
+    test_samples['estimated_nutrients'] = test_samples['gpt-response'].apply(
+        lambda x: {k: v for k, v in sorted(json.loads(x).items(), key=lambda item: item[0])})
+    keys_to_export = ['item_description', 'nutrients',
+                      'estimated_nutrients', 'correct', 'reason']
     json_result = test_samples[keys_to_export].to_dict(orient='records')
     with open('eval_result.json', 'w') as f:
         json.dump(json_result, f, indent=4)
 
-    
     accuracy = test_samples['correct'].mean()
 
     # Print the accuracy
@@ -121,4 +133,3 @@ if __name__ == "__main__":
     for nutrient in total_squared_error.keys():
         mean_squared_error = total_squared_error[nutrient] / len(data)
         print(f'Mean Squared Error for {nutrient}: {mean_squared_error}')
-
--- src/home.tsx
+++ src/home.tsx
@@ -9,6 +9,35 @@ import { useMemo, useState } from "react";
 import eval_result from "../scripts/eval_result.json";
 import { ms } from "../utils/functions";
 
+const categories = {
+  all: 8000,
+  "american indian/alaska native foods": 165,
+  "baby foods": 367,
+  "baked products": 879,
+  "beef products": 961,
+  beverages: 371,
+  "breakfast cereals": 356,
+  "cereal grains and pasta": 181,
+  "dairy and egg products": 283,
+  "fast foods": 363,
+  "fats and oils": 220,
+  "finfish and shellfish products": 265,
+  "fruits and fruit juices": 360,
+  "lamb, veal, and game products": 464,
+  "legumes and legume products": 381,
+  "meals, entrees, and side dishes": 125,
+  "nut and seed products": 137,
+  "pork products": 341,
+  "poultry products": 389,
+  "restaurant foods": 109,
+  "sausages and luncheon meats": 170,
+  snacks: 177,
+  "soups, sauces, and gravies": 465,
+  "spices and herbs": 64,
+  sweets: 360,
+  "vegetables and vegetable products": 836,
+};
+
 interface Food {
   true: number;
   predicted: number;
@@ -25,6 +54,7 @@ const byError = pipe(
 function Home() {
   const [selectedValue, setSelectedValue] = useState<O.Option<any>>(O.none);
   const [macro, setMacro] = useState<Macro>("calories");
+  const [selectedCategory, setSelectedCategory] = useState<any>("all");
 
   const values = useMemo(
     () =>
@@ -60,60 +90,79 @@ function Home() {
 
   return (
     <div className="page">
-      <div className="tabs">
-        {["calories", "carbohydrates", "proteins", "fats"].map((tab: any) => (
+      <div className="categories">
+        {Object.keys(categories).map((category) => (
           <div
-            className={cx("tab", { ["selected"]: tab === macro })}
+            key={category}
+            className={cx("category", {
+              selected: category === selectedCategory,
+            })}
             onClick={() => {
-              setMacro(tab);
+              setSelectedCategory(category);
             }}
           >
-            {tab}
+            {category}
           </div>
         ))}
       </div>
 
-      <div className="values">
-        {values.map((value, index) => (
-          <div
-            key={index}
-            className="value"
-            onMouseEnter={() => {
-              setSelectedValue(O.some(value));
-            }}
-            onMouseLeave={() => {
-              setSelectedValue(O.none);
-            }}
-          >
+      <div className="workspace">
+        <div className="tabs">
+          {["calories", "carbohydrates", "proteins", "fats"].map((tab: any) => (
             <div
-              className="dot true"
-              style={{ bottom: valueToPosition(value.true) }}
-            />
-            <div
-              className="dot predicted"
-              style={{ bottom: valueToPosition(value.predicted) }}
-            />
-            <div
-              className="error"
-              style={{
-                bottom:
-                  valueToPosition(Math.min(value.predicted, value.true)) +
-                  ms(-5) / 2,
-                height: Math.abs(
-                  valueToPosition(value.true) - valueToPosition(value.predicted)
-                ),
+              className={cx("tab", { ["selected"]: tab === macro })}
+              onClick={() => {
+                setMacro(tab);
               }}
-            />
-          </div>
-        ))}
-      </div>
-
-      {O.isSome(selectedValue) && (
-        <div className="quicklook">
-          <div>{selectedValue.value.description}</div>
-          <div>{selectedValue.value.error}</div>
+            >
+              {tab}
+            </div>
+          ))}
         </div>
-      )}
+
+        <div className="values">
+          {values.map((value, index) => (
+            <div
+              key={index}
+              className="value"
+              onMouseEnter={() => {
+                setSelectedValue(O.some(value));
+              }}
+              onMouseLeave={() => {
+                setSelectedValue(O.none);
+              }}
+            >
+              <div
+                className="dot true"
+                style={{ bottom: valueToPosition(value.true) }}
+              />
+              <div
+                className="dot predicted"
+                style={{ bottom: valueToPosition(value.predicted) }}
+              />
+              <div
+                className="error"
+                style={{
+                  bottom:
+                    valueToPosition(Math.min(value.predicted, value.true)) +
+                    ms(-5) / 2,
+                  height: Math.abs(
+                    valueToPosition(value.true) -
+                      valueToPosition(value.predicted)
+                  ),
+                }}
+              />
+            </div>
+          ))}
+        </div>
+
+        {O.isSome(selectedValue) && (
+          <div className="quicklook">
+            <div>{selectedValue.value.description}</div>
+            <div>{selectedValue.value.error.toFixed(2)}</div>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
